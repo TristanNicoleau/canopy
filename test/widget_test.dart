794ab0973d6a1ba95e45ef57ffe9973d6f63b122
Tristan Nicoleau <tristan.nicoleau@energie-perspective.fr>
2021-02-14 23:46:05 +0100

    Add PlantsListPage widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:canopy_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(CanopyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
